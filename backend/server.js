a4da7b36975ecbfdf0822b50eb1d74e4cd0846a0
AvivEldad <avivelded@gmail.com>
2023-12-25 21:19:22 +0200

    DB configuration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 
 const port = process.env.port || 3000;
+
+const DB = process.env.DATABASE;
+mongoose.connect(DB).then((con) => {
+  console.log("Connection is successful!");
+});
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}`);
