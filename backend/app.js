f84a8b76a1a661944c4c8e90f88285bf7ec3bcdb
AvivEldad <avivelded@gmail.com>
2023-12-26 21:40:44 +0200

    add item routes to app

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const itemRouter = require("./routes/itemRoutes");
 
 const app = express();
 
 app.use(express.json());
+
+//Routes
+app.use("/api/v1/items", itemRouter);
 
 module.exports = app;
