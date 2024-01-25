640da767090bd69bbfbb23a08becb96ed08e8f4e
AvivEldad <avivelded@gmail.com>
2024-01-25 15:21:56 +0200

    get all users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,22 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!",
-  });
+const User = require("../models/userModel");
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).json({
+      status: "success",
+      results: users.length,
+      data: { users },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
