c469fbac96e961d031aca18782cb70e7099430df
AvivEldad <avivelded@gmail.com>
2024-02-01 21:28:20 +0200

    update user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,15 @@
 const User = require("../models/userModel");
+const AppError = require("./../Utils/appError");
+
+const filteredBody = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+  return newObj;
+};
 
 exports.getAllUsers = async (req, res) => {
   try {
@@ -40,4 +51,33 @@ exports.deleteUser = (req, res) => {
     status: "error",
     message: "This route is not yet defined!",
   });
+};
+
+exports.updateMe = async (req, res, next) => {
+  if (req.body.password || req.body.confirmPassword) {
+    return next(new AppError("This route is not for password update", 400));
+  }
+  try {
+    const filteredBody = filterObj(req, bod, "name", "email");
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user.id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        user: updatedUser,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
 };
