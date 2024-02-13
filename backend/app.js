8820d3a654b20a3f7f805c99367aafc555ceba9b
AvivEldad <avivelded@gmail.com>
2024-02-13 21:54:12 +0200

    hpp package

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
+const hpp = require("hpp");
 const AppError = require("./utils/appError");
 
 const app = express();
@@ -24,6 +25,11 @@ app.use(express.json({ limit: "10kb" }));
 //Data Sanitization
 app.use(mongoSanitize());
 app.use(xss());
+app.use(
+  hpp({
+    whitelist: [],
+  })
+);
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
