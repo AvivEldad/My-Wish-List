da51223a594dff555bbe9647709bf8ce258f58f4
AvivEldad <avivelded@gmail.com>
2024-02-21 21:26:57 +0200

    add handler factory

---
+++ backend/controllers/handlerFactory.js
@@ -0,0 +1,24 @@
+const { validationResult } = require("express-validator");
+
+exports.deleteOne = (Model) => async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const id = req.params.userId || req.params.itemId || req.params.id;
+    const doc = await Model.findByIdAndDelete(id);
+    if (!doc) {
+      throw new Error("There is no such doc");
+    }
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
