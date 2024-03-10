d964929aacee01f56d3850995685b2d286656ec7
AvivEldad <avivelded@gmail.com>
2024-03-10 21:44:48 +0200

    mail upgrade

--- Utils/email.js
+++ Utils/email.js
@@ -1,23 +1,37 @@
 const nodemailer = require("nodemailer");
+const htmlToText = require("html-to-text");
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      password: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Aviv Eldad <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOptions = {
-    from: "Aviv Eldad <avivelded@gmail.com>",
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  createTransport() {
+    //use sendGrid
+  }
 
-  await transporter.sendMail(mailOptions);
+  async send(template, subject) {
+    //need to add pug.renderFile()
+    const html = "<h1> text </h1>";
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    await this.createTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to your WishList");
+  }
+
+  async sendPasswordReset() {
+    await this.send("passwordReset", "Reset your password");
+  }
 };
-
-module.exports = sendEmail;
